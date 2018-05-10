d9fc7d827bbc9501620244380f2e65e008b93b11
Kevin Walsh <kevinwalsh322@gmail.com>
2018-05-10 12:13:58 -0400

    too many changes to list whatever

--- testcv.py
+++ testcv.py
@@ -15,10 +15,9 @@ def matrix_similarity(a,b):
 	return truths / len(c)
 
 
-
 # BEGIN VIDEO CAPTURE
 try:
-	cap = cv2.VideoCapture(0)  # CHOOSE VIDEO SOURCE
+	cap = cv2.VideoCapture('videoplayback.mp4')  # CHOOSE VIDEO SOURCE
 
 	counter_brightness = 0
 	
@@ -30,13 +29,20 @@ try:
 		'top_right': [], 'bottom_right': []}
 
 	# Capture loop
-
 	while(True):
 	    # capture frame-by-frame
 	    ret, frame = cap.read()
 
 	    # operate on frame
 	    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+	    h = gray.shape[1]
+	    v = gray.shape[0]
+
+	    cv2.rectangle(gray, (0,0), (40,40), 255, 3)
+	    cv2.rectangle(gray, (h,0), (h - 40,40), 255, 3)
+	    cv2.rectangle(gray, (0,v - 40), (40,v), 255, 3)
+	    cv2.rectangle(gray, (h,v - 40), (h - 40, v), 255, 3)
 
 	    counter_brightness += 1
 
@@ -84,26 +90,29 @@ try:
 
 
 	    # DETECT LARGE DIFFERENCES IN CORNER VALUES
+	    counter_pov += 1
+
 	    if counter_pov == 0:
-	    	top_left = gray[0:40][0:40]
-	    	bottom_left = gray[0:40][-40:]
-	    	top_right = gray[-40:][0:40]
-	    	bottom_right = gray[-40:][-40:]
+	    	top_left = frame[0:40][0:40]
+	    	bottom_left = frame[0:40][-40:]
+	    	top_right = frame[-40:][0:40]
+	    	bottom_right = frame[-40:][-40:]
 	    	if len(corners['top_left']) > 0:
 	    		tl_diff = matrix_similarity(top_left, corners['top_left'][-1])
 	    		tr_diff = matrix_similarity(top_right, corners['top_right'][-1])
 	    		bl_diff = matrix_similarity(bottom_left, corners['bottom_left'][-1])
 	    		br_diff = matrix_similarity(bottom_right, corners['bottom_right'][-1])
-	    		avg = sum([tl_diff, tr_diff, bl_diff, br_diff])/4
-	    		print(max([tl_diff, tr_diff, bl_diff, br_diff]))
-
-
+	    		
+	    		closest = max([tl_diff, tr_diff, bl_diff, br_diff])
+	    		if closest < 0.025:
+	    			now = datetime.datetime.now()
+	    			logMsg = "{0:02d}:{1:02d}:{2:02d} -- Camera May Have Been Moved".format(now.hour,now.minute,now.second)
+	    			addLog(logMsg)
 
 	    	corners['top_left'].append(top_left)
 	    	corners['top_right'].append(top_right)
 	    	corners['bottom_left'].append(bottom_left)
 	    	corners['bottom_right'].append(bottom_right)
-
 
 	    if counter_pov >= 120:
 	    	counter_pov = -1
@@ -112,8 +121,6 @@ try:
 	    	corners['top_right'] = []
 	    	corners['bottom_left'] = []
 	    	corners['bottom_right'] = []
-
-	    counter_pov += 1
 
 
 
